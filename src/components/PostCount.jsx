c4a5ed3fc80fb6dd374dfd5df665e89cdb397455
67160024 <67160024@go.buu.ac.th>
2026-03-17 18:32:02 +0700

    activity1:PostCount Component

---
+++ src/components/PostCount.jsx
@@ -0,0 +1,9 @@
+function PostCount({ count }) {
+  return (
+    <p style={{ marginBottom: "1rem", color: "#4a5568" }}>
+      โพสต์ทั้งหมด: {count} รายการ
+    </p>
+  );
+}
+
+export default PostCount;
